95a3ee00ede917a13bf703cabd65300f6a57fddd
ALIK MOLDOVANU <al1k24@icloud.com>
2021-02-17 18:26:57 +0200

    Empty project

---
+++ CS-MyArena/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CS-MyArena
+//
+//  Created by Алик on 17.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
